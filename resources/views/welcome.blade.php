0d26e8d02c82b6fca876736e97cdd99790806040
Hartawan Bahari M <182410101113@students.unej.ac.id>
2020-10-10 18:47:48 +0700

    Menambah tampilan maintenance
    Co-Authored-By: noobiesdev <69758991+noobiesdev@users.noreply.github.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kedele.Me</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -13,6 +13,10 @@
         <style>
             html, body {
                 background-color: #fff;
+                background-image: url({{ asset('assets/images/page.svg')}});
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -25,7 +29,7 @@
             }
 
             .flex-center {
-                align-items: center;
+                /* align-items: center; */
                 display: flex;
                 justify-content: center;
             }
@@ -45,7 +49,8 @@
             }
 
             .title {
-                font-size: 84px;
+                margin-top: 35%;
+                font-size: 64px;
             }
 
             .links > a {
@@ -81,10 +86,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <div class='console-container'><span id='text'></span></div>
                 </div>
 
-                <div class="links">
+                <!-- <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -92,8 +97,61 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> -->
             </div>
         </div>
+        <script>
+      // function([string1, string2],target id,[color1,color2])
+      consoleText(['Under Maintenance', 'Dalam Perbaikan'], 'text',['blue', 'black']);
+
+      function consoleText(words, id, colors) {
+        if (colors === undefined) colors = ['#fff'];
+        var visible = true;
+        var con = document.getElementById('console');
+        var letterCount = 1;
+        var x = 1;
+        var waiting = false;
+        var target = document.getElementById(id)
+        target.setAttribute('style', 'color:' + colors[0])
+        window.setInterval(function() {
+
+          if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = words[0].substring(0, letterCount)
+            window.setTimeout(function() {
+              var usedColor = colors.shift();
+              colors.push(usedColor);
+              var usedWord = words.shift();
+              words.push(usedWord);
+              x = 1;
+              target.setAttribute('style', 'color:' + colors[0])
+              letterCount += x;
+              waiting = false;
+            }, 100)
+          } else if (letterCount === words[0].length + 1 && waiting === false) {
+            waiting = true;
+            window.setTimeout(function() {
+              x = -1;
+              letterCount += x;
+              waiting = false;
+            }, 100)
+          } else if (waiting === false) {
+            target.innerHTML = words[0].substring(0, letterCount)
+            letterCount += x;
+          }
+        }, 120)
+        window.setInterval(function() {
+          if (visible === true) {
+            con.className = 'console-underscore hidden'
+            visible = false;
+
+          } else {
+            con.className = 'console-underscore'
+
+            visible = true;
+          }
+        }, 400)
+      }
+    </script>
     </body>
 </html>
